65b0ba6215a96ac13053a65a3a2cf090329d940c
sander <82777720+hsanderr@users.noreply.github.com>
2023-05-31 12:12:17 -0300

    Update MyProject.c

--- Prat_3/src/MyProject.c
+++ Prat_3/src/MyProject.c
@@ -1,17 +1,17 @@
 /*
 
-  Parte 1: 
+  Parte 1:
         utilizar libs ADC e LCD
-        ler tensão na entrada RA (0, 1, 2, 3 ou 5)
-        AN2 e AN3 são para Vref externos
+        ler tens?o na entrada RA (0, 1, 2, 3 ou 5)
+        AN2 e AN3 s?o para Vref externos
         mostrar valor lido na linha 1 do LCD na forma AN5 = x.xx V
-        
+
   Parte 2:
         medir entrada do LM35 (RE1 ou RE2)
-        ajustar Vref para faixa de tensão medida (usar a função FVR do PIC)
-        curva teórica: 10mV / ºC
-        mostrar valor lido na linha 2 do LCD na forma xx.x ºC
-        
+        ajustar Vref para faixa de tens?o medida (usar a fun??o FVR do PIC)
+        curva te?rica: 10mV / ?C
+        mostrar valor lido na linha 2 do LCD na forma xx.x ?C
+
   Value                    Description
   _ADC_INTERNAL_REF        Both voltage references set to internal VDD and VSS.
   _ADC_EXTERNAL_REF        Both voltage references set to external Vref+ and Vref- pins.
@@ -28,25 +28,25 @@
 
   Van = Vb * Vrefp / (2^n - 1)
   Vb = Van * (2^n - 1) / Vrefp
-  
-  Se não funcionar, configurar registradores PVCFG, NVCFG e FVRS
+
+  Se n?o funcionar, configurar registradores PVCFG, NVCFG e FVRS
 
 */
 
 #define ADC_VCHANNEL 0
 #define ADC_TCHANNEL 6
-#define START_DELAY_MS 3000
-#define LOOP_DELAY_MS 3000
+#define START_DELAY_MS 1000
+#define LOOP_DELAY_MS 1000
 #define VREFP 1.024
 #define TWO_POWER_TEN 1024
 
-// T = a * V => a = T / V = 1 / (10 * 10^(-3)) = 100 ºC / V
+// T = a * V => a = T / V = 1 / (10 * 10^(-3)) = 100 ?C / V
 #define LINEAR_COEF 100
 
 unsigned int ad_voltage;
 unsigned int ad_temp;
-float ad_voltage_an;
-float ad_temp_an;
+double ad_voltage_an;
+double ad_temp_an;
 int use_get_sample = 1;
 
 // Lcd pinout settings
@@ -78,10 +78,11 @@ void main() {
 //  ADFM_bit = 1;              // Right justified
 
   ADC_Init_Advanced(_ADC_INTERNAL_VREFL | _ADC_INTERNAL_FVRH1);
+//  ADC_Init();
   LCD_Init();
   Lcd_Cmd(_LCD_CLEAR);
   Lcd_Cmd(_LCD_CURSOR_OFF);
-  LCD_Out(1,1,"Prática 3...");
+  LCD_Out(1,1,"Pratica 3...");
   delay_ms(START_DELAY_MS);
   while(1)
   {
@@ -90,26 +91,30 @@ void main() {
       Lcd_Cmd(_LCD_CLEAR);
       LCD_Out(1,1,"ADC_Get_Sample");
       ad_voltage = ADC_Get_Sample(ADC_VCHANNEL);
-      ad_voltage_an = ad_voltage * VREFP / (TWO_POWER_TEN - 1);
+      ad_voltage_an = ((ad_voltage * VREFP) / (TWO_POWER_TEN - 1));
       ad_temp = ADC_Get_Sample(ADC_TCHANNEL);
       ad_temp_an = LINEAR_COEF * ad_temp * VREFP / (TWO_POWER_TEN - 1);
-      use_get_sample = 0;
+//      use_get_sample = 0;
     }
     else
     {
       Lcd_Cmd(_LCD_CLEAR);
       LCD_Out(1,1,"ADC_Read");
       ad_voltage = ADC_Read(ADC_VCHANNEL);
-      ad_voltage_an = ad_voltage * (TWO_POWER_TEN - 1) / VREFP;
+      ad_voltage_an = (double)(((double)(((double)ad_voltage) * VREFP)) / (TWO_POWER_TEN - 1));
+//      ad_voltage_an = ((((ad_voltage) * VREFP)) / (TWO_POWER_TEN - 1));
       ad_temp = ADC_Read(ADC_TCHANNEL);
-      ad_temp_an = LINEAR_COEF * ad_temp;
+      ad_temp_an = (double)((double)(LINEAR_COEF * ((double)ad_temp) * VREFP) / (TWO_POWER_TEN - 1));
       use_get_sample = 1;
     }
-    delay_ms(1000);
-    sprintf(voltage_txt, "AN%d=%.2fV", ADC_VCHANNEL, ad_voltage_an);
-    sprintf(temp_txt, "AN%d=%.1fºC", ADC_TCHANNEL, ad_voltage_an);
+    delay_ms(500);
+    sprintf(voltage_txt, "AN0=%3.2fV", ad_voltage_an);
+    sprintf(temp_txt, "AN6=%3.1fÂºC", ad_temp_an);
+//    sprintf(voltage_txt, "AN0=%dV", ADC_VCHANNEL, ad_voltage);
+//    sprintf(temp_txt, "AN6=%dC", ADC_TCHANNEL, ad_voltage);
     Lcd_Cmd(_LCD_CLEAR);
     LCD_Out(1,1,voltage_txt);
+    LCD_Out(2,1,temp_txt);
     delay_ms(LOOP_DELAY_MS);
   }
 }
